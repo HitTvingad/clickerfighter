05cdbed33a908f92165733a4b821774d44cf476f
Rasmus Kjellander <rasmus.kjellander@learnet.se>
2024-01-18 11:17:18 +0100

    Unity init

---
+++ Assets/Scripts/CurrencyOnClick.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CurrencyOnClick : MonoBehaviour
+{
+    
+    void Start()
+    {
+        Button CurrencyButton = GetComponent("The Currency Button") as Button;
+    }
+    
+    public int AddCurrency()
+    {
+        
+    }
+}
